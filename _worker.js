a6990332c6582600e0177f6d0e1a281a3be4d724
ttrrttttteeeertt <167129917+ttrrttttteeeertt@users.noreply.github.com>
2024-05-14 12:46:32 +0330

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,35 @@
+const API_HOST = "example.com" // Change to "example.com" for the EU region
+   /**
+    * @param {API_HOST: string} env
+    /*
+async function handleRequest(event) {
+    const url = new URL(event.request.url)
+    const pathname = url.pathname
+    const search = url.search
+    const pathWithParams = pathname + search
+    if (pathname.startsWith("/static/")) {
+        return retrieveStatic(event, pathWithParams)
+    } else {
+        return forwardRequest(event, pathWithParams)
+    }
+}
+
+async function retrieveStatic(event, pathname) {
+    let response = await caches.default.match(event.request)
+    if (!response) {
+        response = await fetch(`https://${API_HOST}${pathname}`)
+        event.waitUntil(caches.default.put(event.request, response.clone()))
+    }
+    return response
+}
+
+async function forwardRequest(event, pathWithSearch) {
+    const request = new Request(event.request)
+    request.headers.delete("cookie")
+    return await fetch(`https://${API_HOST}${pathWithSearch}`, request)
+}
+
+addEventListener("fetch", (event) => {
+    event.passThroughOnException()
+    event.respondWith(handleRequest(event))
+})
